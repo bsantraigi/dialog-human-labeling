3a219924803ae51e84b45f40403ac386edb9634f
Bishal <bsantraigi@gmail.com>
2019-10-27 16:01:09 +0530

    Fixed the consolidation lambda(post)

--- sagemaker-labelling/s3obj-textdata-postfunc.py
+++ sagemaker-labelling/s3obj-textdata-postfunc.py
@@ -1,12 +1,33 @@
 import json
 import sys
 from s3_helper import S3Client
+import boto3
+import re
+
+
+"""
+lambda_handler receives following event object
+{
+  "version": "2018-10-06",
+  "labelingJobArn": "arn:aws:sagemaker:us-east-1:986216541610:labeling-job/dialog-labeling-demo9-private",
+  "payload": {
+    "s3Uri": "s3://sagemaker-dialog-label-demo/dialog-labeling-demo9-private/annotations/consolidated-annotation/consolidation-request/iteration-1/2019-10-27_08:56:48.json"
+  },
+  "labelAttributeName": "dialog-labeling-demo9-private",
+  "roleArn": "arn:aws:iam::986216541610:role/service-role/AmazonSageMaker-ExecutionRole-20191027T040862",
+  "outputConfig": "s3://sagemaker-dialog-label-demo/dialog-labeling-demo9-private/annotations"
+}
+
+
+
+"""
+
+def get_object(s3, s3Uri):
+    m = re.match(r"s3:\/\/(.+?)\/(.+)", s3Uri)
+    bucket, key = m.group(1), m.group(2)
+    return s3.get_object(Bucket=bucket, Key=key)["Body"].read().decode()
 
 def lambda_handler(event, context):
-
-    # Event received
-    print("Received event: " + json.dumps(event, indent=2))
-
     labeling_job_arn = event["labelingJobArn"]
     label_attribute_name = event["labelAttributeName"]
 
@@ -18,7 +39,7 @@ def lambda_handler(event, context):
     payload = event["payload"]
     role_arn = event["roleArn"]
 
-    output_config = None # Output s3 location. You can choose to write your annotation to this location
+    output_config = None  # Output s3 location. You can choose to write your annotation to this location
     if "outputConfig" in event:
         output_config = event["outputConfig"]
 
@@ -28,14 +49,29 @@ def lambda_handler(event, context):
     if "kmsKeyId" in event:
         kms_key_id = event["kmsKeyId"]
 
+    s3 = boto3.client('s3')
+    # try:
+    s3_ref = payload['s3Uri']
+
+    consolidation_request = json.loads(get_object(s3, s3_ref))
+
+    print("Consolidation Request:", json.dumps(consolidation_request, indent=2))
+
+    # return {
+    #     'event': event,
+    #     'bucket': bucket,
+    #     'key': key,
+    #     'request': json.dumps(consolidation_request, indent="2")
+    # }
+
     # Create s3 client object
-    s3_client = S3Client(role_arn, kms_key_id)
+    # s3_client = S3Client(role_arn, kms_key_id)
 
     # Perform consolidation
-    return do_consolidation(labeling_job_arn, payload, label_attribute_name, s3_client)
+    return do_consolidation(labeling_job_arn, consolidation_request, label_attribute_name, s3)
 
 
-def do_consolidation(labeling_job_arn, payload, label_attribute_name, s3_client):
+def do_consolidation(labeling_job_arn, payload, label_attribute_name, s3):
     """
         Core Logic for consolidation
 
@@ -45,12 +81,6 @@ def do_consolidation(labeling_job_arn, payload, label_attribute_name, s3_client)
     :param s3_client: S3 helper class
     :return: output JSON string
     """
-
-    # Extract payload data
-    if "s3Uri" in payload:
-        s3_ref = payload["s3Uri"]
-        payload = json.loads(s3_client.get_object_from_s3(s3_ref))
-        print(payload)
 
     # Payload data contains a list of data objects.
     # Iterate over it to consolidate annotations for individual data object.
@@ -73,8 +103,7 @@ def do_consolidation(labeling_job_arn, payload, label_attribute_name, s3_client)
                 worker_id = annotations[i]["workerId"]
                 annotation_content = annotations[i]['annotationData'].get('content')
                 annotation_s3_uri = annotations[i]['annotationData'].get('s3uri')
-                annotation = annotation_content if annotation_s3_uri is None else s3_client.get_object_from_s3(
-                    annotation_s3_uri)
+                annotation = annotation_content if annotation_s3_uri is None else get_object(s3, annotation_s3_uri)
                 annotation_from_single_worker = json.loads(annotation)
 
                 print("{} Received Annotations from worker [{}] is [{}]"
@@ -82,7 +111,7 @@ def do_consolidation(labeling_job_arn, payload, label_attribute_name, s3_client)
 
             # Notice that, no consolidation is performed, worker responses are combined and appended to final output
             # You can put your consolidation logic here
-            consolidated_annotation = {"annotationsFromAllWorkers": annotations} # TODO : Add your consolidation logic
+            consolidated_annotation = {"annotationsFromAllWorkers": annotations}  # TODO : Add your consolidation logic
 
             # Build consolidation response object for an individual data object
             response = {
@@ -112,3 +141,4 @@ def do_consolidation(labeling_job_arn, payload, label_attribute_name, s3_client)
     print(consolidated_output)
     print(" ------------------------- ")
     return consolidated_output
+
