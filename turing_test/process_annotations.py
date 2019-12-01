acedccf9b994e6ce847a9ce64e02859901b2e18d
Bishal Santra <bsantraigi@gmail.com>
2019-12-01 11:30:21 +0530

    sync

--- turing_test/process_annotations.py
+++ turing_test/process_annotations.py
@@ -4,6 +4,7 @@ import json
 from collections import defaultdict, Counter
 
 annotations = defaultdict(list)
+annotations_by_id = {}
 for afile in glob.glob('annotation_download/multiwoz_turing/*.json'):
     # print(afile)
     json_arr = json.loads(open(afile).read())
@@ -15,10 +16,13 @@ for afile in glob.glob('annotation_download/multiwoz_turing/*.json'):
             for key, val in worker_annot.items():
                 worker_annot[key] = json.loads(val)
 
+            worker_id = real_annot['workerId']
+            annotations_by_id[worker_id] = []
             for key in worker_annot['annotations']:
                 ann = worker_annot['annotations'][key]
                 gt = worker_annot['ground_truths'][key]
                 annotations[key].append((gt, ann))
+                annotations_by_id[worker_id].append((gt, ann))
 
 annotations = dict(annotations)
 
@@ -28,10 +32,35 @@ for key in annotations:
         c[instance] += 1
 # print(annotations)
 print(c)
-H_H = c[('human', 'human')]
-H_AI = c[('human', 'ai')]
-AI_H = c[('ai', 'human')]
-AI_AI = c[('ai', 'ai')]
+def average_compare(c):
+    H_H = c[('human', 'human')]
+    H_AI = c[('human', 'ai')]
+    AI_H = c[('ai', 'human')]
+    AI_AI = c[('ai', 'ai')]
 
-print(f"Baseline Ratio:", H_H/(H_H + H_AI))
-print(f"RESGEN Ratio:", AI_H/(AI_AI + AI_H))
+    print(f"Baseline Ratio:", H_H/(H_H + H_AI))
+    print(f"RESGEN Ratio:", AI_H/(AI_AI + AI_H))
+
+average_compare(c)
+
+"""BAD ANNOTATIONS
+1. Any annotation with all 'human' or 'ai' as annotation!
+2. 
+"""
+def personal_compare(c):
+    H_H = c[('human', 'human')]
+    H_AI = c[('human', 'ai')]
+    AI_H = c[('ai', 'human')]
+    AI_AI = c[('ai', 'ai')]
+
+    if (AI_AI == 0 and H_AI == 0) or (AI_H == 0 and H_H==0):
+        return 0
+
+    return AI_H - H_AI
+
+for worker_id in annotations_by_id:
+    annots = annotations_by_id[worker_id]
+    c = Counter(annots)
+    print(worker_id)
+    print(c)
+    print(personal_compare(c))
